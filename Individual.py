3605bb2f233b748f90815e4762b3ad06129573f7
jackreeceejini <jackreeceejini@gmail.com>
2020-05-27 12:26:39 -0400

    Added documentation for the Whole project.

--- Individual.py
+++ Individual.py
@@ -1,3 +1,46 @@
+"""
+Phase 1: The first phase of this project would be to create code that demonstrates the idea of a
+new kind of Global Economy I call DifferentialEconomy where people are given a tiny amount of 
+money every second rather than a bulk sum like is being done during the Covid19 pandemic.
+
+This tiny amount will be a universal basic income. The reason its differential is that it is not constant, 
+and there are things an individual can do to increase how much is coming in per unit time.
+
+As an example, if you are unemployed you could be receiving maybe $1000 dollars a month, this is 
+$12000 a year. But with this new system of differential economy you could just have a tiny amount deposited
+to your account every second of $0.000380469. Eventually every month this will amount to $1000 but it will
+not be deposited at once. Now if you add a new source of income like maybe you start working, your employer 
+will just add a small differential to your base income which is flat and guaranteed for everyone. With the 
+new differntial you will now be earning more. As you add new income sources your differential increases, thus 
+increasing your yearly income. 
+
+Employers will not need to pay you bulk sums every month, week or two weeks, they will just add to your 
+income differential, this will enable small businesses hire faster because you will not need a fixed amount 
+of large capital to hire people.
+
+The hourly pay rate is a great innnovation but the truth is that you do not receive your hourly rate everyday as you
+work. Rather you receive a paycheck once a week or in two weeks depending on the arrangement. But with this new economic 
+system you could be getting money comming in every second and you can decide to spend or to save. 
+
+Taxation can be implemented by reserving a certain amount of your income differential to the IRS. 
+
+This system will assure that everybody survives no matter the economic situation. $1000 a month is just a suggestion 
+based on the current ideas about UBI being floated around. Governments can decide to set this base amount to anything 
+they calculate will be the best.
+
+Phase 2: This second phase will see a reimplementation of the code for real world deployment, this is when performance issues will 
+be considered. The first implementation of the code will be more illustrative. So that people at all levels of experience
+could read the code and understand the system. Phase 1 is basically experimentation of ideas on how such a future Global economy
+could be designed.
+
+Phase 3: If the idea grows successfully, then it will be time for real world experimentation in a Network. In Phase 1 and Phase 2 we
+could experiment on Blockchain ideas to see if it will be appropriate for such a system. By Phase 3 everything is mature and 
+ready for experimentation via the release of an experimental build to the public. With the data obtained on the real world activity
+of the project we could work towards Phase 4
+
+Phase 4: In this phase the system is mature and ready for us to start pitching the idea to Governments and Institutions around
+the world. 
+"""
 from hashlib import sha256
 class Individual:
     def __init__(self, name, dateOfBirth, SSN):
